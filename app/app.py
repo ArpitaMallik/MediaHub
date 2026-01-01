3ac9a9d556ab5f54f030ddc4645eae9d2badb159
Arpita Mallik <arpitamallik13@gmail.com>
2026-01-01 11:28:15 +0600

    initial commit

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile, Depends, Form
+from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+)
